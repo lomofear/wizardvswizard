8cd86ceea9d262333781fc574b5fc13818d774bc
David Martinez Marti <deavidsedice@gmail.com>
2016-01-24 10:10:45 +0100

    mejora de seguimiento del path

--- defense1/defense1.py
+++ defense1/defense1.py
@@ -95,6 +95,72 @@ class GamePath(GameObject):
         pygame.draw.rect(screen, (int(120+math.sin(self.num/23.0)*100),
                                   int(120+math.sin(self.num/11.0)*100),
                                   int(120+math.sin(self.num/4.0)*100)), Rect)
+def coeff_correlacion_vectores(x1,y1,x2,y2):
+    """
+    Coeficiente de correlación entre vectores. Básicamente, obtener un
+    número entre 0 y 1 que nos indique la similitud de la dirección entre
+    ambos. 0, van en direcciones que no tienen nada que ver, 1, van en la misma
+    dirección.
+    
+    Para qué usamos esta función:
+    - Enemigos: Al hacerlos seguir un camino (Path), es interesante que aceleren
+      en las rectas. Para ello hay que saber si van en la dirección correcta o
+      si están girando. Comparando su vector de dirección con "hacia donde queremos
+      que vayan" sabemos si están en momento de aceleración o de frenado.
+    """
+    dist1 = math.hypot(x1,y1)
+    dist2 = math.hypot(x2,y2)
+    if dist1 < 0.00001 or dist2 < 0.00001:
+        # no puede haber o calcularse relación de dirección si uno de los dos
+        # ... no tiene distancia
+        return 0
+    
+    # normalizar v1 y v2:
+    x1 /= dist1
+    y1 /= dist1
+    x2 /= dist2
+    y2 /= dist2
+    
+    # Calculamos el vector de diferencia entre v1 y v2:
+    dx = x1 - x2
+    dy = y1 - y2
+    
+    dist3 = math.hypot(dx,dy)
+    # dist3 - tamaño del vector de diferencia (módulo del vector)
+    #  ... nos indica cuan diferentes son:
+    #  - 0.0 : totalmente iguales
+    #  - 0.5 : 45 grados de diferencia approx.
+    #  - 1.0 : 90 grados de diferencia
+    #  - 2.0 : 180 grados de diferencia.
+    #  
+    #  supuestamente no puede dar ningún valor fuera del rango 0..2 porque:
+    #   - un módulo, distancia u hipotenusa siempre es positivo
+    #   - la mínima distancia posible es cero, cuando el vector es 0,0
+    #   - como ambos módulos son normalizados (distancia 1.0) la máxima 
+    #     distancia posible resulta de que se sumaran las dos y 1 + 1 = 2.
+    #     Para sumar las dos distancias basta con que un vector sea el contrario
+    #     del otro y conseguir, efectivamente, una suma.
+    
+    
+    # En la función estadistica de coeficiente, normalmente funciona del revés:
+    #  - +1 : iguales
+    #  -  0 : sin relación
+    #  - -1 : relación inversa.
+    
+    # Como nos interesa (por similitud) que la salida se asemeje a la función
+    # estadística, mapeamos los valores:
+    
+    coeff = 1 - dist3
+    
+    # nos daria:
+    #     1 - 0.0 =  1.0
+    #     1 - 0.5 =  0.5
+    #     1 - 1.0 =  0.0
+    #     1 - 1.5 = -0.5
+    #     1 - 2.0 = -1.0
+    
+    return coeff
+    
 
 class GameEnemy(GameObject):
     def __init__(self, *args, **kwargs):
@@ -103,6 +169,9 @@ class GameEnemy(GameObject):
         self.x = GamePath.PATH[self.posnum].x + random.randint(-12,12)
         self.y = GamePath.PATH[self.posnum].y + random.randint(-12,12)
         self.posnum += 1
+        self.velocity = 200
+        self.minvelocity = 20
+        self.maxvelocity = 200
 
     def draw(self, screen):
         pygame.draw.circle(screen, (200,80,80), (int(self.x),int(self.y)) , 8)
@@ -117,13 +186,22 @@ class GameEnemy(GameObject):
         dx, dy = dstx - self.x , dsty - self.y
         dst = math.hypot(dx,dy)
         ndx , ndy = dx / dst, dy / dst
-        self.dx += ndx * self.dtime * 60.0
-        self.dy += ndy * self.dtime * 60.0
+        #self.dx += ndx * self.dtime * 60.0
+        #self.dy += ndy * self.dtime * 60.0
         
         self.dx /= 1.5 ** self.dtime
         self.dy /= 1.5 ** self.dtime
+        corr = coeff_correlacion_vectores(self.dx, self.dy, ndx, ndy)
+        if corr < 0: corr = 0 # eliminamos negativos
+        corr_f = corr - 0.90 # 10% accel, 90% frenado.
+        self.velocity *= 1+(corr_f*30.00*self.dtime) # aplicar factor 30x/s
+        self.velocity /= 1.5 ** self.dtime
+        if self.velocity < self.minvelocity: self.velocity = self.minvelocity 
+        if self.velocity > self.maxvelocity: self.velocity = self.maxvelocity 
+        self.dx = (self.dx + ndx * self.velocity) / 2.0
+        self.dy = (self.dy + ndy * self.velocity) / 2.0
         
-        if dst < 16: self.posnum += 1
+        if dst < 12: self.posnum += 1
         dstx,dsty = GamePath.PATH[self.posnum-1].x,GamePath.PATH[self.posnum-1].y
         dx2, dy2 = dstx - self.x , dsty - self.y
         dx2 += dx
