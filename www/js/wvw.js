9a49c15d1ed9f7d70c9724913e4cfce6db94d58e
David Martinez Marti <deavidsedice@gmail.com>
2016-10-11 17:15:52 +0200

    agregar botones nuevos y logotipo con transparencia

--- www/js/wvw.js
+++ www/js/wvw.js
@@ -2,12 +2,15 @@ var Container = PIXI.Container,
     autoDetectRenderer = PIXI.autoDetectRenderer,
     loader = PIXI.loader,
     resources = PIXI.loader.resources,
+    Text = PIXI.Text,
     Sprite = PIXI.Sprite;
 var GAME_WIDTH = 896;
 var GAME_HEIGHT = 512;
 var renderer = autoDetectRenderer(GAME_WIDTH, GAME_HEIGHT); // aspect: 7/4
 var logo;
+var FPS = 30;
 
+renderer.backgroundColor = 0xFFFFFF;
 renderer.view.style.position = "absolute";
 renderer.view.style.display = "block";
 //renderer.autoResize = true;
@@ -20,20 +23,24 @@ document.body.appendChild(renderer.view);
 var stage = new Container();
 
 var loader_rect1 = new PIXI.Graphics();
-loader_rect1.beginFill(0x000060);
-loader_rect1.lineStyle(2, 0xFFFFFF, 1);
-loader_rect1.drawRect(50, 50, 800, 20);
+loader_rect1.beginFill(0xFFFFFF);
+loader_rect1.lineStyle(2, 0x000000, 1);
+loader_rect1.drawRect(49, 49, 802, 22);
 loader_rect1.endFill();
 
 var loader_rect2 = new PIXI.Graphics();
 loader_rect2.beginFill(0x66CCFF);
 loader_rect2.lineStyle(0, 0x0000FF, 0);
-loader_rect2.drawRect(50, 50, 0, 20);
+loader_rect2.drawRect(0, 0, 20, 20);
 loader_rect2.endFill();
+loader_rect2.x = 50
+loader_rect2.y = 50
 stage.addChild(loader_rect1);
 stage.addChild(loader_rect2);
 
 renderer.render(stage);
+
+var sprites = new Object();
 
 function loadProgressHandler(loader, resource) {
 
@@ -43,14 +50,17 @@ function loadProgressHandler(loader, resource) {
     //Display the precentage of files currently loaded
     console.log("progress: " + loader.progress + "%");
 
-    loader_rect2.width = loader.progress * 8;
+    loader_rect2.width = parseInt( 50+ loader.progress * 7.5);
+
     renderer.render(stage);
 }
 
 
 loader
   .add( [
-    "img/logo1.jpg"
+    "img/logo1.png",
+    "img/button1.png",
+    ""
     ]
   )
   .on("progress", loadProgressHandler)
@@ -58,7 +68,7 @@ loader
 
 function setup() {
     logo = new Sprite(
-        loader.resources["img/logo1.jpg"].texture
+        loader.resources["img/logo1.png"].texture
     );
     logo.scale.set(0.25, 0.25);
     logo.anchor.set(0.5, 0.5);
@@ -82,7 +92,8 @@ var game_state = play_intro1;
 function gameLoop(){
 
     //Loop this function 60 times per second
-    requestAnimationFrame(gameLoop);
+    //requestAnimationFrame(gameLoop);
+    setTimeout(gameLoop, 1000 / FPS);
 
     game_state();
 
@@ -90,34 +101,99 @@ function gameLoop(){
     renderer.render(stage);
 }
 
+
 function play_intro1() {
+
+          /*  setup_intro2();
+            game_state = play_intro2;
+*/
     if (logo.scale.x < 1) {
-        logo.scale.x *= 1.01;
-        logo.scale.y *= 1.01;
+        logo.scale.x *= 1.015;
+        logo.scale.y *= 1.015;
     } else {
         logo.scale.x = 1;
         logo.scale.y = 1;
     }
     if (logo.y < GAME_HEIGHT / 2) {
-        logo.rotation += 2.0/60.0;
-        logo.y += 1 + logo.scale.x;
+        logo.rotation += 3.0/60.0;
+        logo.y += 2 + logo.scale.x;
     } else {
         logo.y = GAME_HEIGHT / 2;
         while (logo.rotation > 0.1) logo.rotation -= Math.PI * 2;
         if (logo.rotation < 0) {
-            logo.rotation += 5.0/60.0;
+            logo.rotation += 4.0/60.0;
         } else {
             logo.rotation = 0;
+            setup_intro2();
             game_state = play_intro2;
         }
     }
 
 }
+
+function create_button(text, onDown) {
+    var bg = new Sprite(
+        loader.resources["img/button1.png"].texture
+    );
+    var btext = new PIXI.Text(
+      text,
+      {    fontFamily : 'Arial',
+    fontSize : '36px',
+    fontStyle : 'italic',
+    fontWeight : 'bold',
+    fill : '#F7EDCA'}
+    );
+    bg.anchor.set(0.5, 0.5);
+    bg.x = 0;
+    bg.y = 0;
+
+    btext.emit = function(x,y,z) {};
+    btext.anchor.set(0.5, 0.5);
+    btext.x = 0;
+    btext.y = -2;
+
+    var button = new PIXI.Container();
+    button.addChild(bg);
+    button.addChild(btext);
+    button.bg = bg;
+    button.btext = btext;
+    if (onDown) {
+        bg.interactive = true;
+        bg.on('mousedown', onDown);
+        bg.on('touchstart', onDown);
+        button.onDown = onDown;
+    }
+
+    return button;
+
+}
+
+function menu_button1_onDown() {
+    sprites.menu_button1.alpha = 0.5;
+}
+
+function setup_intro2() {
+    logo.y = GAME_HEIGHT / 2;
+    logo.rotation = 0;
+    logo.scale.set(1,1);
+
+    sprites.menu_button1 = create_button("Start Game", menu_button1_onDown);
+    //sprites.menu_button1.anchor.set(0.5, 0.5);
+    sprites.menu_button1.x = GAME_WIDTH / 2;
+    sprites.menu_button1.y = GAME_HEIGHT / 2 + 32;
+    sprites.menu_button1.alpha = 0;
+    stage.addChild(sprites.menu_button1);
+}
+
 function play_intro2() {
     if (logo.y > GAME_HEIGHT / 4) {
-        logo.y -= 1;
+        logo.y -= 2;
     } else {
         logo.y = GAME_HEIGHT / 4;
+    }
+    if (sprites.menu_button1.alpha < 1 ) {
+        sprites.menu_button1.alpha += 0.02;
+        if (sprites.menu_button1.alpha > 0.99 ) sprites.menu_button1.alpha = 1;
     }
 
 }
